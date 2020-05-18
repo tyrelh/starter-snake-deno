a0fb4d6f0dce736f2a6a6617239d92fb7b8b885d
tyrelh <krankyjoe@gmail.com>
2020-05-17 21:04:36 -0700

    add back the port arg

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: deno run --allow-net --allow-env main.ts
+web: deno run --allow-net:${PORT} --allow-env main.ts --port=${PORT}
--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 import {Application, Router, Request, Response} from "https://deno.land/x/oak/mod.ts";
-// import * as flags from "https://deno.land/std/flags/mod.ts";
+import * as flags from "https://deno.land/std/flags/mod.ts";
 
 const env = Deno.env.toObject();
-// const argPort = flags.parse(Deno.args).port;
-// const PORT = Number(argPort) || Number(env.PORT) || 5000;
-const PORT = Number(env.PORT) || 5000;
+const argPort = flags.parse(Deno.args).port;
+console.log("port: ", argPort);
+const PORT = Number(argPort) || Number(env.PORT) || 5000;
+// const PORT = Number(env.PORT) || 5000;
 
 export const start = ({request, response}: {request: Request, response: Response}): void => {
     response.body = {

0b670e65d76fe93a7fde9574c168c4f6fb75e6b5
tyrelh <krankyjoe@gmail.com>
2020-05-23 09:51:11 -0700

    update to battlesnake API v1

--- main.ts
+++ main.ts
@@ -1,20 +1,30 @@
-import {Application, Router, Request, Response} from "https://deno.land/x/oak/mod.ts";
+import { Application, Router, Request, Response } from "https://deno.land/x/oak/mod.ts";
 import * as flags from "https://deno.land/std/flags/mod.ts";
+import { GameRequest } from "./types.ts";
 
 const argPort = flags.parse(Deno.args).port;
 const PORT = Number(argPort) || 5000;
 
-export const start = ({request, response}: {request: Request, response: Response}): void => {
+export const root = ({response}: {response: Response}): void => {
     response.body = {
+        apiversion: "1",
+        author: "<YOUR_GITHUB_USERNAME>",
         color: "#AA22FF",
-        headType: "beluga",
-        tailType: "bolt"
+        head: "beluga",
+        tail: "bolt"
     };
     response.status = 200;
 }
 
-export const move = ({request, response}: {request: Request, response: Response}): void => {
+export const start = ({request, response}: {request: Request, response: Response}): void => {
+    response.status = 200;
+}
+
+export const move = async ({request, response}: {request: Request, response: Response}): void => {
+    const requestBody = await request.body();
+    const gameRequest: GameRequest = requestBody.value;
     // Start with your logic here!
+    
     response.body = { move: "right" };
     response.status = 200;
 }
@@ -23,15 +33,11 @@ export const end = ({request, response}: {request: Request, response: Response})
     response.status = 200;
 }
 
-export const ping = ({response}: {response: Response}): void => {
-    response.status = 200;
-}
-
 const router = new Router();
 router.post("/start", start);
 router.post("/move", move);
 router.post("/end", end);
-router.get("/ping", ping);
+router.get("/", root);
 
 const app = new Application();
 app.use(router.routes());
